0883c3ac9f5ad72e25ea4127b0ae0849f1215e42
Mehdi Cheracher <mehdi.cheracher@gmail.com>
2020-03-29 00:35:46 +0000

    add inliner structure

--- inliner.go
+++ inliner.go
@@ -1,4 +1,134 @@
 package main
 
-func Inline() {}
+import (
+	"bufio"
+	"io"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+)
 
+var IncludeRegex = regexp.MustCompile("#include\\s+\"(?P<path>.*?)\"")
+var IncludeRegexStd = regexp.MustCompile("#include\\s+<(.+)>")
+
+type ReaderProvider interface {
+	GetReader(string) (io.Reader, error)
+}
+type DefaultReaderProvider struct {
+}
+
+func (rp *DefaultReaderProvider) GetReader(path string) (io.Reader, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	return bufio.NewReader(file), nil
+}
+
+type Inliner struct {
+	includeDir string
+	excludes   []*regexp.Regexp
+}
+
+type Link struct {
+	from string
+	to   string
+}
+
+func readContent(path string, provider ReaderProvider) string {
+	reader, _ := provider.GetReader(path)
+	scanner := bufio.NewScanner(reader)
+	content := ""
+	for scanner.Scan() {
+		content += scanner.Text()
+		content += "\n"
+	}
+	return content
+}
+
+// Extract links
+// currentRoot should always be a directory.
+func ExtractLinks(currentRoot string, content string, provider ReaderProvider) []Link {
+	result := make([]Link, 0)
+	set := NewSet()
+	extractLinks(currentRoot, content, &set, &result, provider)
+	return result
+}
+
+func extractLinks(currentRoot string, content string, set *Set, result *[]Link, provider ReaderProvider) {
+	if set.Has(currentRoot) {
+		return
+	}
+	set.Add(currentRoot)
+	lines := strings.Split(content, "\n")
+	for _, line := range lines {
+		if IncludeRegex.MatchString(line) {
+			filePath := IncludeRegex.FindStringSubmatch(line)[1]
+			newFilePath := path.Join(currentRoot, filePath)
+			*result = append(*result, Link{currentRoot, newFilePath})
+			newFileContent := readContent(newFilePath, provider)
+			extractLinks(path.Dir(newFilePath), newFileContent, set, result, provider)
+		}
+	}
+}
+
+// Creates a new instance of the Inliner type.
+// includeDir: the root directory of your cpp library.
+// excludes: regular expressions denoting file paths to ignore from inlining
+func NewInliner(includeDir string, excludes []string) (*Inliner, error) {
+	n := len(excludes)
+	excludesRegex := make([]*regexp.Regexp, n)
+	for i, value := range excludes {
+		compiledRegex, err := regexp.Compile(value)
+		if err != nil {
+			return nil, err
+		}
+		excludesRegex[i] = compiledRegex
+	}
+	return &Inliner{includeDir, excludesRegex}, nil
+}
+
+// Returns true if the current line starts with a #include
+func isIncludeLine(line string) bool {
+	return strings.HasPrefix(strings.TrimLeft(line, " "), "#include")
+}
+
+// Returns a string aggregating all the contents of the files given by the argument array.
+func filesContent(order []string) string {
+	return ""
+}
+
+func (inliner *Inliner) Inline(reader io.Reader) (string, error) {
+	scanner := bufio.NewScanner(reader)
+	lines := make([]string, 0)
+	seenIncludes := NewSet()
+	insertPosition, lineNumber := 0, 0
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+		if !IncludeRegex.MatchString(currentLine) {
+			lines = append(lines, currentLine)
+		}
+		if isIncludeLine(currentLine) && IncludeRegexStd.MatchString(currentLine) {
+			libraryName := IncludeRegexStd.FindStringSubmatch(currentLine)[1]
+			seenIncludes.Add(libraryName)
+			insertPosition = lineNumber
+		}
+		lineNumber++
+	}
+	provider := DefaultReaderProvider{}
+	links := ExtractLinks(inliner.includeDir, strings.Join(lines, "\n"), &provider)
+	graph := NewGraph(links, seenIncludes)
+	fileOrder := graph.GetTopologicalOrder(inliner.includeDir)
+	content := ""
+	// a -> b
+	// b -> c
+	// a -> d
+	// c -> d
+	for i, _ := range lines {
+		if i == insertPosition {
+			content += filesContent(fileOrder)
+		}
+	}
+	return content, nil
+}
--- inliner_test.go
+++ inliner_test.go
@@ -1,1 +1,87 @@
 package main
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"strings"
+	"testing"
+)
+
+type MockProvider struct{}
+type MockProviderMulti struct{}
+
+func (pr *MockProvider) GetReader(path string) (io.Reader, error) {
+	if path == "include/strings.h" {
+		return strings.NewReader(""), nil
+	}
+	if path == "include/lib/constants.h" {
+		return strings.NewReader(""), nil
+	}
+	if path == "include/lib/tree.h" {
+		return strings.NewReader(""), nil
+	}
+	return nil, errors.New("Failed to find given path")
+}
+
+func (pr *MockProviderMulti) GetReader(path string) (io.Reader, error) {
+	if path == "include/lib/tree.h" {
+		return strings.NewReader(`#include "../io/tree_reader.h"`), nil
+	}
+	if path == "include/lib/constants.h" {
+		return strings.NewReader(""), nil
+	}
+	if path == "include/tree_reader.h" {
+		return strings.NewReader(""), nil
+	}
+	if path == "include/io/tree_reader.h" {
+		return strings.NewReader(""), nil
+	}
+	return nil, errors.New("Failed to find given path")
+}
+
+func TestExtractLinks(t *testing.T) {
+	// #include "something.(h|hpp|cc|cpp)"
+	// #include "something
+	// #include "%something.hpp"
+	// #include "(../)/something*/something.(h|hpp|cc|cpp)"
+	provider := &MockProvider{}
+	links := ExtractLinks("include", `
+		#include "strings.h"
+		#include "lib/constants.h"
+		#include "lib/tree.h"
+		#include <iostream>
+    `, provider)
+	assert.Equal(t, 3, len(links))
+	assert.Equal(t, "include/strings.h", links[0].to)
+	assert.Equal(t, "include/lib/constants.h", links[1].to)
+	assert.Equal(t, "include/lib/tree.h", links[2].to)
+}
+
+func TestExtractLinksMultiLevel(t *testing.T) {
+	// #include "something.(h|hpp|cc|cpp)"
+	// #include "something
+	// #include "%something.hpp"
+	// #include "(../)/something*/something.(h|hpp|cc|cpp)"
+	provider := &MockProviderMulti{}
+	links := ExtractLinks("include", `
+		#include "lib/tree.h"
+		#include "lib/constants.h"
+		#include <iostream>
+    `, provider)
+	assert.Equal(t, 3, len(links))
+	assert.Equal(t, "include/lib/tree.h", links[0].to)
+	assert.Equal(t, "include/io/tree_reader.h", links[1].to)
+	assert.Equal(t, "include/lib/constants.h", links[2].to)
+}
+
+func TestExtractLinksNoLinks(t *testing.T) {
+
+	provider := &MockProvider{}
+	links := ExtractLinks("include", `
+		#include <vector>
+		#include <iostream>
+    `, provider)
+
+	assert.Equal(t, 0, len(links))
+}
